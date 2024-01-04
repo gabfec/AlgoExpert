061d1b4cb9e4da4e955dc5bbef582266cc4a18df
Gabriel Feceoru <gabi.feceoru@gmail.com>
2024-01-04 20:46:47 +0100

    middle-node

---
+++ Easy/middle-node.py
@@ -0,0 +1,16 @@
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def middleNode(linkedList):
+    fast = linkedList
+    slow = linkedList
+
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+
+    return slow
