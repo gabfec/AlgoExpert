148b5a6ef914b186f4d3c3035f98b414a3da2c8f
Gabriel Feceoru <gabi.feceoru@gmail.com>
2024-01-07 20:37:56 +0100

    Add another solution to missing-numbers

--- Medium/missing-numbers.py
+++ Medium/missing-numbers.py
@@ -1,3 +1,4 @@
+#O(n) time | O(n) space
 def missingNumbers(nums):
     missing = []
     all_nums = [False] * (len(nums) + 2)
@@ -7,6 +8,22 @@ def missingNumbers(nums):
 
     for i in range(len(all_nums)):
         if all_nums[i] == False:
+            missing.append(i + 1)
+
+    return missing
+
+
+#O(n) time | O(1) space
+def missingNumbers2(nums):
+    nums += [float('inf'), float('inf')]
+
+    for i in range(len(nums) - 2):
+        idx = abs(nums[i]) - 1
+        nums[idx] = -nums[idx]
+
+    missing = []
+    for i in range(len(nums)):
+        if nums[i] > 0:
             missing.append(i + 1)
 
     return missing
